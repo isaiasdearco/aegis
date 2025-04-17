30a249e732b0e8566d413735e7e57fafa2b7c04d
isaiasdearco <isaiasdearco@users.noreply.github.com>
2025-04-17 16:55:46 +0000

    deploy: 014eccfd281e4523a25d46d6a793358c9c20cad3

--- assets/index-CB8xRC6H.js
+++ assets/index-CB8xRC6H.js
@@ -6,7 +6,7 @@
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${n}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Welcome to Aegis AI 3</h1>
+    <h1>Welcome to Aegis AI 4</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
